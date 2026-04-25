003e452f25288a3e61e63cc5cce2c1e5fbaf13f8
nico-amsterdam <n.g.c.hoogervort@gmail.com>
2026-04-26 00:58:19 +0200

    Upgrade ex_doc

--- mix.exs
+++ mix.exs
@@ -40,8 +40,8 @@ defmodule SimpleMemCache.Mixfile do
   # Type "mix help deps" for more examples and options
   defp deps do
     [
-      {:ex_doc, "~> 0.34.2", only: :dev, runtime: false},
-      {:credo,  "~> 1.7"   , only: :dev, runtime: false}
+      {:ex_doc, "~> 0.40", only: :dev, runtime: false},
+      {:credo,  "~> 1.7" , only: :dev, runtime: false}
     ]
   end
 
